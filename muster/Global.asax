9aa5d19b5b407fc635e5ffe50319a1c26d3f479f
unknown <JJohnston@devlab.hq.ondemandanalysis.com>
2016-03-23 11:01:55 -0700

    Initial Commit (already a ways in)

---
+++ muster/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="muster.MvcApplication" Language="C#" %>
---
+++ muster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(muster.Startup))]
+namespace muster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
